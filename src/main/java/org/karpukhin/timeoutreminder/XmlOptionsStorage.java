6a7dd4666a5f3e52b65cd0a8f4d7e11ace98bdab
Pavel Karpukhin <karpukhin.pavel@gmail.com>
2015-09-10 18:39:36 +0300

    Issue #4 First commit

---
+++ src/main/java/org/karpukhin/timeoutreminder/XmlOptionsStorage.java
@@ -0,0 +1,19 @@
+package org.karpukhin.timeoutreminder;
+
+/**
+ *
+ *
+ * @author Pavel Karpukhin
+ * @since 23.07.15
+ */
+public class XmlOptionsStorage implements OptionsStorage {
+
+    @Override
+    public Options load() {
+        return null;
+    }
+
+    @Override
+    public void save(Options options) {
+    }
+}
